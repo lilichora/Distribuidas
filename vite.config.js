01125e2f2e9d4dabbe2ac3dc4a0b0032cd857c0f
Liliana Del Carmen Chora Monar <116900601+lilichora@users.noreply.github.com>
2024-08-01 15:12:32 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/Distribuidas/'
+  base: '/Distribuidas/',
+  server: {
+    proxy: {
+      // Redirige todas las solicitudes a /api a la API HTTP
+      '/api': {
+        target: 'http://18.221.39.240', // URL de tu API
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
